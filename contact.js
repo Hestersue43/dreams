3c05b93ba11b6dde87721b28046d6669873ad07c
veldamart <31259069+veldamart@users.noreply.github.com>
2017-09-18 15:04:39 -0400

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,1 +1,43 @@
+Var firstname=name
+if (firstname.indexOf('') >=0){
+	firstname=name.split('').slice(0. -1).join('');
+	
+}
+
+$.ajax ({
+	url: "//formspree.io/dreams.veldamart@gmail.com",
+	type: "POST",
+	dataType: "json",
+	data: {
+		name: name,
+		phone: phone,
+		email: email,
+		message:message
+	},
+	cache: false,
+	success function(){
+	$('#success).html ("div class-'alert alert-success'>");
+	$('#success > .alert-success').html("<button type='button' class= 'close' data-dismiss='alert' aria-hidden='true'>$times ;")
+		.append("</button>");
+	$('#success).alert-success')
+		.append("<strong>Your message has been sent</strong>");
+	$('#success).alert-success')	
+		.append("</div>");
+	}
+	
+	error function(){
+	$('#success).html ("div class-'alert alert-danger'>");
+	$('#success > .alert-danger').html("<button type='button' class= 'close' data-dismiss='alert' aria-hidden='true'>$times ;")
+		.append("</button>");
+	$('#success).alert-danger').append("<strong>'+ firstName + ", It seems that my mail server is not responding. Please try again later'</strong>");
+	$('#success).alert-danger').append("</div>");
+	//clear all fields
+	$('#contactForm').trigger("reset");
+	
+	}
+})
+},
+filter function(){
+	return $(this).is(":visisble");
+}
 
