51ee36d01f49dc71517c6f201fe6df89b762b039
veldamart <31259069+veldamart@users.noreply.github.com>
2017-09-18 20:24:03 -0400

    Update contact.js

--- contact.js
+++ contact.js
@@ -16,31 +16,45 @@ $.ajax ({
 	},
 	cache: false,
 	success function(){
-	$('#success).html ("<div class='alert alert-success'>");
-	$('#success > .alert-success').html("<button type='button' class= 'close' data-dismiss='alert' aria-hidden='true'>$times ;")
-		.append("</button>");
-	$('#success).alert-success')
-		.append("<strong>Your message has been sent</strong>");
-	$('#success).alert-success')	
-		.append("</div>");
-	}
-	
-	error function(){
-	$('#success).html ("<div class-'alert alert-danger'>");
-	$('#success > .alert-danger').html("<button type='button' class= 'close' data-dismiss='alert' aria-hidden='true'>$times ;")
-		.append("</button>");
-	$('#success).alert-danger').append("<strong>'+ firstName + ", It seems that my mail server is not responding. Please try again later'</strong>");
-	$('#success).alert-danger').append("</div>");
+	$("#btnSubmit").attr("disableb", false);
+	$('#success').html ("<div class='alert alert-success'>");
+	$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+	.append("</button>");
+	$('#success > .alert-success')
+	.append("<strong>Your message has been sent</strong>");
+	$('#success > .alert-success')
+	.append("</div>");
 	//clear all fields
 	$('#contactForm').trigger("reset");
-	
-	}
-})
-},
-filter function(){
+	},
+	error function(){
+	//Fail message
+	$('#success).html ("<div class-'alert alert-danger'>");
+	$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times ;")
+	.append("</button>");
+	$('#success>.alert-danger').append("<strong>Sorry"+ firstName + ", It seems that my mail server is not responding. Please try again later!</strong>");
+	$('#success>.alert-danger').append("</div>");
+	//clear all fields
+	$('#contactForm').trigger("reset");
+	},
+      })
+     },
+        filter function(){
 	return $(this).is(":visisble");
-}
-}
+     },
+   });
+
+$("a[data-toggle=\"tab\").click(function(e) {
+  e.preventDefault();
+ $(this).tab("show");
+});
+});
+?/when clicking on Full hide fail/success boxes
+$('#name).focus(function(){
+$('#success).html(function(");
+ });
+  
+
 
 
 
