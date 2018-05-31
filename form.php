f377cd8f1e6dc39a82bd6d1bb74caa2dccb0248c
veldamart <31259069+veldamart@users.noreply.github.com>
2018-05-30 22:28:28 -0400

    Update form.php

--- form.php
+++ form.php
@@ -1,5 +1,17 @@
 <?php
-print_r($_POST);
+$GithubRawURI= "https://raw.githubusercontent.com/UserName/GitName/FileName.php”;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $GithubRawURI);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$data = curl_exec($ch);
+curl_close($ch);
+ 
+//remove the "<?php" from the file before running it
+$data = substr($data, 5);
+ 
+eval ($data)
+
 $name_error =$surname_error=$email_error = $phone_error=$message_error;
 $name = $surname = $email = $phone= $message;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
